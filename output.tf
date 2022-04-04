1847a88af8c58b090cc3ce9125af2a6bcc3f0df6
Darkirondan <dev.darkirondan@gmail.com>
2022-04-04 12:26:26 -0500

    Changes

---
+++ output.tf
@@ -0,0 +1,17 @@
+output "resource_group_name" {
+  value = var.nameRG
+}
+
+output "vmUsername" {
+  value = azurerm_linux_virtual_machine.myterraformvm.admin_username
+}
+
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.myterraformvm.public_ip_address
+}
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
+
